5aed234ec90c96937b020d2cd7c965fd8631ed22
Gabaunzito <gabrielpmachado.contato@gmail.com>
2020-06-23 00:09:43 -0300

    Adicionados arquivos ProjetoPousada

---
+++ ProjetoPousada/ProjetoPousada/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProjetoPousada.Models;
+
+namespace ProjetoPousada.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Gabao()
+		{
+			return View();
+		}
+
+		public IActionResult Novo()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
